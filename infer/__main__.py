0b4798144046da89920577d8778c2a2b866d0f0a
anime-song <52844453+anime-song@users.noreply.github.com>
2025-03-19 13:05:41 +0900

    Update __main__.py

--- infer/__main__.py
+++ infer/__main__.py
@@ -19,18 +19,19 @@ SV_SAMPLER = SVSampler()
 def main(args):
     path_model = args.path_model or config.path.apc
     device = torch.device(args.device) if args.device else DEVICE_DEFAULT
-    pipeline = Pipeline(path_model, device)
+    pipeline = Pipeline(path_model, device, with_sv=False)
 
     src = args.input
     if src.startswith("https://"):
         src = download(src)
 
-    sv = SV_SAMPLER.sample(params=args.style)
-    pipeline.wav2midi(src, args.output, sv, silent=False)
+    # sv = SV_SAMPLER.sample(params=args.style)
+    pipeline.wav2midi(src, args.output, None, silent=False)
 
 
 def download(url):
     from yt_dlp import YoutubeDL
+
     ydl_opts = {
         "outtmpl": "_audio.%(ext)s",
         "format": "bestaudio/best",
@@ -50,9 +51,23 @@ def download(url):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("input", type=str, help="Path to the input wav file or URL of YouTube video")
-    parser.add_argument("-o", "--output", type=str, default="output.mid", help="Path to the output midi file. Defaults to 'output.mid'")
-    parser.add_argument("-s", "--style", type=str, default="level2", help="Cover style. Valid values are 'level1', 'level2', and 'level3'. Defaults to 'level2'")
+    parser.add_argument(
+        "input", type=str, help="Path to the input wav file or URL of YouTube video"
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=str,
+        default="output.mid",
+        help="Path to the output midi file. Defaults to 'output.mid'",
+    )
+    parser.add_argument(
+        "-s",
+        "--style",
+        type=str,
+        default="level2",
+        help="Cover style. Valid values are 'level1', 'level2', and 'level3'. Defaults to 'level2'",
+    )
     parser.add_argument("--path_model", type=str, default=None)
     parser.add_argument("--device", type=str, default=None)
     args = parser.parse_args()
